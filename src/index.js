695c7020dd42ee6a044b7be1102f6a6e22a7db5e
craffaele <christopher.raffaele@vidmob.com>
2023-03-26 15:51:45 -0400

    rebuild index file to match version 052

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Button from './components/Button.js'
 
 import PlaybackControls from './components/PlaybackControls.js'
@@ -10,41 +9,27 @@ import PrevButton from './components/PrevButton.js'
 import ProgressBar from './components/ProgressBar.js'
 import FormattedTime from './components/FormattedTime.js'
 import TimeMarker, { TimeMarkerType } from './components/TimeMarker.js'
-
 import SoundOnButton from './components/SoundOnButton.js'
 import SoundOffButton from './components/SoundOffButton.js'
 import MuteToggleButton from './components/MuteToggleButton.js'
-
 import VolumeSlider from './components/VolumeSlider.js'
-
 import { ControlDirection } from './components/RangeControlOverlay'
-
 export * from './components/icons.js'
 
 export {
   Button,
-
   PlaybackControls,
   PlayButton,
   PauseButton,
   NextButton,
   PrevButton,
-
   ProgressBar,
   FormattedTime,
   TimeMarker,
   TimeMarkerType,
-
   SoundOnButton,
   SoundOffButton,
   MuteToggleButton,
-
   VolumeSlider,
-=======
-import FormattedTime from './components/FormattedTime.js'
-import * as PlayerIcon from './components/icons.js'
-import Slider from './components/Slider.js'
->>>>>>> c1473cdf748dcf2489ea1d1eff077def935d7e17
-
-export { Direction } from './constants.js'
-export { FormattedTime, PlayerIcon, Slider }
+  ControlDirection,
+}
